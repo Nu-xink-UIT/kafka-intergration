7f1e0163fcac56e3645cf3e5999d57bff7c1d06f
Anhhh3953 <ngocanhdoan1092005@gmail.com>
2026-04-07 11:34:38 +0700

    fix: goldprice topics

--- src/goldprice/core/config.py
+++ src/goldprice/core/config.py
@@ -6,22 +6,15 @@ class Settings:
     KAFKA_BOOTSTRAP_SERVER = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
 
     # topic list
-    TOPIC_RATES = 'goldprice.rates.raw'
-    TOPIC_PERFORMANCE = 'goldprice.performance.raw'
+    TOPIC_RATES = 'goldprice.raw'
 
-    # top 30 currency code influenced Vietnam
-    TIER_1_CURRENCIES = ['USD', 'VND', 'CNY', 'EUR', 'GBP', 'JPY', 'KRW', 'SGD', 'HKD', 'TWD']
-    TIER_2_CURRENCIES = ['THB', 'MYR', 'GBP', 'CHF', 'AUD', 'CAD', 'NZD', 'INR', 'IDR', 'PHP', 'VND', 'KHR', 'LAK', 'AED', 'SAR', 'QAR', 'KWD', 'BHD', 'OMR', 'RUB', 'TRY', 'PLN']
-
+    # top 20 currency code influenced Vietnam
+    CURRENCIES = ['VND', 'AUD', 'CAD', 'CHF', 'CNY', 'DKK', 'EUR', 'GBP', 'HKD', 'INR', 'JPY', 'KRW', 'KWD', 'MYR', 'NOK', 'RUB', 'SAR', 'SEK', 'SGD', 'THB', 'USD']
     # pollingin terval
-    INTERVAL_REALTIME_T1 = 60
-    INTERVAL_REALTIME_T2 = 60 * 5
-    INTERVAL_PERFORMANCE = 3600 * 6 # 6 hours
-    INTERVAL_YEARLY = 3600 * 24
+    INTERVAL_REALTIME = 60
 
     # URL bases
     URL_RATES_BASE = "https://data-asg.goldprice.org/dbXRates/"
-    URL_PERFORMANCE_BASE = "https://goldprice.org/performance-json/"
 
     @staticmethod
     def get_ny_timestamp():
