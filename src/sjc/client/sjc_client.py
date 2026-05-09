61614296f7b55a8ca6b5d2fee9889f6f854eec66
MinhTienNguyen05 <mintinnguyen@gmai.com>
2026-05-09 14:44:07 +0700

    fix: client

--- src/sjc/client/sjc_client.py
+++ src/sjc/client/sjc_client.py
@@ -18,23 +18,15 @@ class GoldPriceClient:
             'Referer': 'https://sjc.com.vn'
         }
     async def fetch(self) -> dict:
-        # 1. Lấy địa chỉ Proxy SOCKS5 từ biến môi trường (hoặc dùng mặc định 4000)
-        proxy_url = os.getenv("HTTP_PROXY", "socks5://127.0.0.1:4000")
+        proxy_url = os.getenv("HTTP_PROXY", "socks5h://127.0.0.1:4000")
 
-        # 2. Khởi tạo SOCKS5 Connector
         connector = ProxyConnector.from_url(proxy_url)
-
-        # 3. Ép ClientSession sử dụng Connector này
         async with aiohttp.ClientSession(headers=self.header, connector=connector) as session:
-
-            # (MẸO ANTI-WAF): Gọi trang chủ 1 lần để hệ thống nhả Session Cookie
-            # Rất nhiều trang như SJC sẽ chặn API nếu bạn chưa từng ghé thăm trang chủ
             try:
                 await session.get("https://sjc.com.vn", ssl=self.ssl_context)
             except Exception as e:
                 print(f"Lỗi khi mồi cookie: {e}")
 
-            # 4. Gọi API thực tế
             async with session.get(self.url, ssl=self.ssl_context) as response:
                 response.raise_for_status()
                 return await response.json(content_type=None)
